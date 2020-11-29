4bd284ed0e9f4693cfe7960cc2b1d82d9b409fc1
jgonzalez-m <jorgej.gonzalezmartinez@gmail.com>
2020-11-29 03:04:20 -0300

    carpetas faltantes agregadas

---
+++ server/middlewares/autenticacion.js
@@ -0,0 +1,45 @@
+const jwt = require('jsonwebtoken');
+//======================
+// verificar token
+//======================
+
+let verificaToken = (req, res, next)=>{
+
+    let token = req.get('token');//en 'token' colocar el nombre de el header que envia el token
+    jwt.verify(token,process.env.SEED, (err,decoded)=>{
+        if(err){
+            return res.status(401).json({
+                ok:false,
+                err
+            });
+        };
+
+        req.usuario = decoded.usuario;
+        next();
+    });
+   
+};
+
+//======================
+// verificar administrador
+//======================
+let verificaAdmin_Role = (req,res,next)=>{
+    let usuario = req.usuario;
+
+    if(usuario.role === 'ADMIN_ROLE'){
+        next();
+    }else{
+        return res.json({
+            ok:false,
+            err: {
+                message: 'El usario no es administrador'
+            }
+        });
+    };
+
+    
+};
+module.exports= {
+    verificaToken,
+    verificaAdmin_Role
+};
